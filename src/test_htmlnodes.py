349154b414ed0e610fdb42ad01f60fca2531f95a
tejjm <uttej.madhusudhan@gmail.com>
2026-05-01 16:08:06 +0000

    Added test cases for extract link and images codes

--- src/test_htmlnodes.py
+++ src/test_htmlnodes.py
@@ -1,7 +1,7 @@
 import unittest
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, TextType, text_node_to_html_node
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images,extract_markdown_links
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_none(self):
@@ -136,3 +136,34 @@ class TestHTMLNode(unittest.TestCase):
         node = TextNode("_This is a text with a italic words right here,which ended now_",TextType.ITALIC)
         expected = [TextNode("_This is a text with a italic words right here,which ended now_",TextType.ITALIC)]
         self.assertEqual(split_nodes_delimiter([node],"_",TextType.ITALIC),expected)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_text_miltiple(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertEqual([("to boot dev","https://www.boot.dev"),("to youtube","https://www.youtube.com/@bootdotdev")],matches)
+
+
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![uwu](https://uwu.com/uwu.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png"),("uwu","https://uwu.com/uwu.png")], matches)
+
+
+    def test_extract_invalid_with_image(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_images(text)
+        self.assertEqual([],matches)
+
+
+
+    def test_extract_invalid_with_text(self):
+        text =  "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![uwu](https://uwu.com/uwu.png)"
+        matches = extract_markdown_links(text)
+        self.assertEqual([],matches)
--- src/textnode.py
+++ src/textnode.py
@@ -31,3 +31,4 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img","",{"src":text_node.url,"alt":text_node.text}) 
     raise ValueError(f"unknown text type: {text_node.text_type}")
+
