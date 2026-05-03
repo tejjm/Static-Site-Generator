bb6bd991beb3f29084d9df8597875e61c9c5f58c
tejjm <uttej.madhusudhan@gmail.com>
2026-05-03 10:09:19 +0000

    added test cases for blocktype

--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from split_blocks import markdown_to_blocks
+from split_blocks import markdown_to_blocks, BlockType,block_to_block_type
 
 
 
@@ -71,3 +71,29 @@ This is the same paragraph on a new line
             blocks = markdown_to_blocks(md)
             self.assertEqual(blocks,[])
 
+
+        def test_block_type_heading(self):
+            block = "## This is a correct heading"
+            expected = BlockType.HEADING
+            self.assertEqual(expected,block_to_block_type(block))
+
+        def test_code_type(self):
+             block = "```\n This is a code block \n```"
+             expected = BlockType.CODE
+             self.assertEqual(expected,block_to_block_type(block))
+            
+        def test_quote_type(self):
+             block = ">This is a quote \n>This is also a quote \n>This is also another quote"
+             expected = BlockType.QUOTE
+             self.assertEqual(expected,block_to_block_type(block))
+
+        def test_unordered_list(self):
+             block = "- This is first but unordered\n- This is second but unordered"
+             expected = BlockType.UNORDERED_LIST
+             self.assertEqual(expected,block_to_block_type(block))
+
+        def test_ordered_list(self):
+             block = "1. Whats up \n2. Nothing you tell me \n3. Nothing from my end"
+             expected = BlockType.ORDERED_LIST
+             self.assertEqual(expected,block_to_block_type(block))
+        
